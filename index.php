13bc6ca47e3b4b271d702224c13a0e790f5bdf4b
Kouki <koukih@umich.edu>
2022-01-29 20:18:55 -0500

    First draft of About page

--- index.php
+++ index.php
@@ -1,7 +1,53 @@
 <?php get_header(); ?>
 
-<div id="page-content">
-  HOME
+<div class="section">
+  <img src="<?php echo get_template_directory_uri(); ?>/cleanup3.jpg" class="center">
+  <div class="About">
+    <div class="Who">
+      <h1>Who Are We?</h1>
+      <p>The Detroit Parks Coalition (DPC) is an alliance of organizations that connect Detroiters to beautiful, healthy, and well-maintained parks & public spaces.</p>
+    </div>
+    <br>
+
+
+    <div class="missions">
+
+
+      <div class="mission1">
+      <p>Our coalition was formed in 2019, when five parks organizations came together to share resources and learning.
+        Since then, we have more than doubled our membership and strengthened our relationships with partners suchas the City of Detroist,
+        Huron-Clinton metroparks Authroity, and Wayne County Parks. Today,
+        we are poised to increase resources to all parks in Detroit and ensure that they are equitably distributed.</p>
+    </div>
+    <br>
+    <div class="mission2">
+      <p>As we grow, we hope to share knowledge, solve problems together, advocate for parks citywide,
+        and ultimately foster strong and supportive relationships among the CIty, park stewardship groups, residents, park users, and partners.</p>
+    </div>
+
+  </div>
 </div>
 
-<?php get_footer(); ?>
+<div class="Goals">
+  <h1>Our Goals</h1>
+
+  <div class="Containers">
+    <div id="box1">
+      <p>Create better experiences for park users through expanded and coordinated 
+        programming, improved, maintenance, and capital improvements</p>
+    </div>
+    <div id="box2">
+      <p>Maximize the impact of park stewardship organizations by building a network, gainging, economies of scale, 
+        and strengthening connections and communication across the parks system.</p>
+    </div>
+    <div id="box3">
+      <p>Advocate for development of new and increased sources of funding for all parks and public spaces in Detroit.</p>
+    </div>
+  </div>
+
+
+
+
+</div>
+
+</div>
