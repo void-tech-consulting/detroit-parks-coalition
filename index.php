021b6be3b77785f8accf640213f3a14cabd85452
Kouki <koukih@umich.edu>
2022-02-13 17:05:00 -0500

    Updated About Us styling

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 
 <div class="section">
-  <img src="<?php echo get_template_directory_uri(); ?>/cleanup3.jpg" class="center">
+  <img src="<?php echo get_template_directory_uri(); ?>/rouge.jpg" class="mainImage">
   <div class="About">
-    <div class="Who">
+    <div class="whoMessage">
       <h1>Who Are We?</h1>
       <p>The Detroit Parks Coalition (DPC) is an alliance of organizations that connect Detroiters to beautiful, healthy, and well-maintained parks & public spaces.</p>
     </div>
@@ -14,40 +14,49 @@
 
 
       <div class="mission1">
-      <p>Our coalition was formed in 2019, when five parks organizations came together to share resources and learning.
-        Since then, we have more than doubled our membership and strengthened our relationships with partners suchas the City of Detroist,
-        Huron-Clinton metroparks Authroity, and Wayne County Parks. Today,
-        we are poised to increase resources to all parks in Detroit and ensure that they are equitably distributed.</p>
-    </div>
-    <br>
-    <div class="mission2">
-      <p>As we grow, we hope to share knowledge, solve problems together, advocate for parks citywide,
-        and ultimately foster strong and supportive relationships among the CIty, park stewardship groups, residents, park users, and partners.</p>
-    </div>
+        <p>Our coalition was formed in 2019, when five parks organizations came together to share resources and learning.
+          Since then, we have more than doubled our membership and strengthened our relationships with partners suchas the City of Detroit,
+          Huron-Clinton Metroparks Authority, and Wayne County Parks. Today,
+          we are poised to increase resources to all parks in Detroit and ensure that they are equitably distributed.</p>
+      </div>
+      <br>
+      <div class="mission2">
+        <p>As we grow, we hope to share knowledge, solve problems together, advocate for parks citywide,
+          and ultimately foster strong and supportive relationships among the City, park stewardship groups, residents, park users, and partners.</p>
+      </div>
 
-  </div>
-</div>
-
-<div class="Goals">
-  <h1>Our Goals</h1>
-
-  <div class="Containers">
-    <div id="box1">
-      <p>Create better experiences for park users through expanded and coordinated 
-        programming, improved, maintenance, and capital improvements</p>
-    </div>
-    <div id="box2">
-      <p>Maximize the impact of park stewardship organizations by building a network, gainging, economies of scale, 
-        and strengthening connections and communication across the parks system.</p>
-    </div>
-    <div id="box3">
-      <p>Advocate for development of new and increased sources of funding for all parks and public spaces in Detroit.</p>
     </div>
   </div>
 
+  <div class="goals">
+    <h1>Our Goals</h1>
+
+    <div class="containers">
+      <div class="goalsLeftBox">
+        <img src="<?php echo get_template_directory_uri(); ?>/Group (1).png">
+        <div class="goalsLeftBoxText">
+          <p>Create better experiences for park users through expanded and coordinated
+            programming, improved, maintenance, and capital improvements</p>
+        </div>
+      </div>
+      <div class="goalsMiddleBox">
+        <div class="goalsMiddleBoxText">
+          <img src="<?php echo get_template_directory_uri(); ?>/Vector.png">
+          <p>Maximize the impact of park stewardship organizations by building a network, gainging, economies of scale,
+            and strengthening connections and communication across the parks system.</p>
+        </div>
+      </div>
+      <div class="goalsRightBox">
+        <div class="goalsRightBoxText">
+          <img src="<?php echo get_template_directory_uri(); ?>/Group.png">
+          <p>Advocate for development of new and increased sources of funding for all parks and public spaces in Detroit.</p>
+        </div>
+      </div>
+    </div>
 
 
 
-</div>
+
+  </div>
 
 </div>
